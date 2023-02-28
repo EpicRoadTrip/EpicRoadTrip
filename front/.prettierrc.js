1729d3f1ef59cbffadd69b70dbe3f9898e742e71
Marius <marius.glo@protonmail.com>
2023-02-28 16:13:50 +0100

    Feature/prettier2 (#9)
    * Prettier: adding Prettier for front
    
    * SearchBar: Adding new searchbar components (#6)
    
    * :test_tube: Front: Adding failing test for SearchBar
    
    * :sparkles: Adding empty SearchBar component
    
    * :package: SearchBar: Adding new packages for searchbar
    
    ---------
    
    Co-authored-by: Mathis <mathis@chouic.com>
    Co-authored-by: Guska <60897185+GildasGonz@users.noreply.github.com>

---
+++ front/.prettierrc.js
@@ -0,0 +1,13 @@
+module.exports = {
+  arrowParens: 'avoid',
+  bracketSameLine: true,
+  bracketSpacing: true,
+  singleQuote: true,
+  trailingComma: 'all',
+  semi: false,
+  printWidth: 100,
+  bracketSpacing: true,
+  trailingComma: 'es5',
+  parser: 'typescript',
+  tabWidth: 2,
+}
