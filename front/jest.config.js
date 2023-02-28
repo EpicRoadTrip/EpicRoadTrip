b8ded40d3f75d5c9edf02aca88a77a8a700debd9
Marius <marius.glo@protonmail.com>
2023-02-28 16:30:45 +0100

    Update src folder in test

--- front/jest.config.js
+++ front/jest.config.js
@@ -24,12 +24,7 @@ const customJestConfig = {
     },
     testEnvironment: 'jest-environment-jsdom',
     "collectCoverage": true,
-    "collectCoverageFrom": ["./src/**"],
-    "coverageThreshold": {
-        "global": {
-            "lines": 90
-        }
-    }
+    "collectCoverageFrom": ["./src/**"]
 }
 
 // createJestConfig is exported this way to ensure that next/jest can load the Next.js config which is async
