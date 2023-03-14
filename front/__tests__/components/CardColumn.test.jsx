3434a0b8a011446b680ae853f90696e4c875e501
Voltaire-Perrochaud <68203869+Voltaire-Perrochaud@users.noreply.github.com>
2023-03-14 15:55:20 +0100

    Component/card (#28)
    * Add CardRow test
    
    Test CardRow component
    
    * Add CardColumn test
    
    Test CardColumn component
    
    * Add global var color
    
    * Add card column css module
    
    * Update test
    
    * Add card interface
    
    * Add CardColumn component
    
    * Update CardRow test
    
    * Update CardRow test
    
    Add click test that redirect on detail page
    
    * Add next-router-mock / Add testing-library/user-event
    
    * Update CardRow test
    Test redirect to detail page on click on card
    
    * Add id type number to card interface
    
    * Add CardRow component
    
    * Update Cards test
    
    * Update CardColumn
    
    * Fix import cleanup
    
    * Remove unused import
    
    * Fix AfterEach callback function
    
    * Update Cards
    Use Image component from next/image
    
    ---------
    
    Co-authored-by: Marius <marius.glo@protonmail.com>

---
+++ front/__tests__/components/CardColumn.test.jsx
@@ -0,0 +1,47 @@
+import { render, screen, cleanup } from '@testing-library/react';
+import '@testing-library/jest-dom';
+import CardColumn from '../../src/components/CardColumn';
+
+jest.mock('next/router', () => require('next-router-mock'));
+afterEach(cleanup);
+
+describe('Card column', () => {
+    it('Renders correctly', () => {
+        render(<CardColumn alt='Photo de montagne' title='La montagne du Gay' price={542} description='La description' />);
+
+        expect(screen.getByTestId('card-container')).toBeVisible();
+        expect(screen.getByTestId('card-image')).toBeVisible();
+        expect(screen.getByTestId('card-header-title')).toBeVisible();
+        expect(screen.getByTestId('card-header-price')).toBeVisible();
+        expect(screen.getByTestId('card-body-description')).toBeVisible();
+    });
+
+    it('Card container should contain image and card body', () => {
+        const card = render(<CardColumn alt='Photo de montagne' title='La montagne du Gay' price={542} description='La description' />);
+        const imageCard = card.getByTestId('card-image');
+        const cardBody = card.getByTestId('card-body');
+
+        expect(card.container).toContainElement(imageCard);
+        expect(card.container).toContainElement(cardBody);
+    });
+
+    it('Card body should contain card header and card text description', () => {
+        const card = render(<CardColumn alt='Photo de montagne' title='La montagne du Gay' price={542} description='La description' />);
+        const cardBody = card.getByTestId('card-body');
+        const cardHeader = card.getByTestId('card-header');
+        const cardTextDescription = card.getByTestId('card-body-description');
+
+        expect(cardBody).toContainElement(cardHeader);
+        expect(cardBody).toContainElement(cardTextDescription);
+    });
+
+    it('Card header should contain card title and card price', () => {
+        const card = render(<CardColumn alt='Photo de montagne' title='La montagne du Gay' price={542} description='La description' />);
+        const cardHeader = card.getByTestId('card-header');
+        const cardTitle = card.getByTestId('card-header-title');
+        const cardPrice = card.getByTestId('card-header-price');
+
+        expect(cardHeader).toContainElement(cardTitle);
+        expect(cardHeader).toContainElement(cardPrice);
+    });
+})
---
+++ front/__tests__/components/CardRow.test.jsx
@@ -0,0 +1,47 @@
+import { cleanup, render, screen } from '@testing-library/react';
+import '@testing-library/jest-dom';
+import CardRow from '../../src/components/CardRow';
+
+jest.mock('next/router', () => require('next-router-mock'));
+afterEach(cleanup);
+
+describe('Card row', () => {
+    it('Renders correctly', () => {
+        render(<CardRow id={12} title='La montagne du Gay' price={542} description='La description' />);
+
+        expect(screen.getByTestId('card-container')).toBeVisible();
+        expect(screen.getByTestId('card-image')).toBeVisible();
+        expect(screen.getByTestId('card-header-title')).toBeVisible();
+        expect(screen.getByTestId('card-header-price')).toBeVisible();
+        expect(screen.getByTestId('card-body-description')).toBeVisible();
+    });
+
+    it('Card container should contain image and card body', () => {
+        const card = render(<CardRow id={12} title='La montagne du Gay' price={542} description='La description' />);
+        const imageCard = card.getByTestId('card-image');
+        const cardBody = card.getByTestId('card-body');
+
+        expect(card.container).toContainElement(imageCard);
+        expect(card.container).toContainElement(cardBody);
+    });
+
+    it('Card body should contain card header and card text description', () => {
+        const card = render(<CardRow id={12} title='La montagne du Gay' price={542} description='La description' />);
+        const cardBody = card.getByTestId('card-body');
+        const cardHeader = card.getByTestId('card-header');
+        const cardTextDescription = card.getByTestId('card-body-description');
+
+        expect(cardBody).toContainElement(cardHeader);
+        expect(cardBody).toContainElement(cardTextDescription);
+    });
+
+    it('Card header should contain card title and card price', () => {
+        const card = render(<CardRow id={12} title='La montagne du Gay' price={542} description='La description' />);
+        const cardHeader = card.getByTestId('card-header');
+        const cardTitle = card.getByTestId('card-header-title');
+        const cardPrice = card.getByTestId('card-header-price');
+
+        expect(cardHeader).toContainElement(cardTitle);
+        expect(cardHeader).toContainElement(cardPrice);
+    });
+});
