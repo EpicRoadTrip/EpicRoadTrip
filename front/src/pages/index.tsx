df0dacce5a6d18e88fe1f5893e0e0b2a1491adb9
GildasGonz <gildas.gonzalez@epitech.eu>
2023-03-20 10:47:37 +0100

    :sparkles: Adding POC of map direction

--- front/src/pages/index.tsx
+++ front/src/pages/index.tsx
@@ -1,9 +1,23 @@
-import Header from '../components/Header';
+import React from 'react';
+import MapComponent from '../components/MapDirection';
+import { ChakraProvider } from '@chakra-ui/react';
+
+const AnyReactComponent = ({ text }) => <div style={{backgroundColor: 'red'}}>{text}</div>;
 
 export default function IndexPage() {
+  const defaultProps = {
+    center: {
+      lat: 10.99835602,
+      lng: 77.01502627
+    },
+    zoom: 11
+  };
+
   return (
     <>
-      <Header data-testid='i-header' />
+      <ChakraProvider>
+        <MapComponent/>
+      </ChakraProvider>
     </>
   )
 }
