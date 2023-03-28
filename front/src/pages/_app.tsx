811f02974ba729bc3a819a1a30895a47a14f5df2
Voltaire-Perrochaud <contact.voltaireperrochaud@gmail.com>
2023-03-28 12:55:32 +0200

    Remove unused var / Type props

--- front/src/pages/_app.tsx
+++ front/src/pages/_app.tsx
@@ -2,8 +2,9 @@ import '../../styles/globals.css';
 
 import { Provider } from 'react-redux';
 import { store } from '../store/store';
+import type { AppProps } from 'next/app';
 
-function MyApp({ Component, pageProps, ...rest }) {
+function MyApp({ Component, pageProps }: AppProps) {
   return (
     <Provider store={store}>
       <Component {...pageProps} />
