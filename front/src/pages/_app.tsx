28818b3e011f18b0a894933c4dd63f2452073f01
Voltaire-Perrochaud <contact.voltaireperrochaud@gmail.com>
2023-04-03 15:56:26 +0200

    Add material ui provider requirement

--- front/src/pages/_app.tsx
+++ front/src/pages/_app.tsx
@@ -1,15 +1,19 @@
-import '../../styles/globals.css';
+import '../../styles/globals.css'
 
-import { Provider } from 'react-redux';
-import { store } from '../store/store';
-import type { AppProps } from 'next/app';
+import { Provider } from 'react-redux'
+import { store } from '../store/store'
+import type { AppProps } from 'next/app'
+import { LocalizationProvider } from '@mui/x-date-pickers'
+import { AdapterMoment } from '@mui/x-date-pickers/AdapterMoment'
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <Provider store={store}>
-      <Component {...pageProps} />
-    </Provider>
-  );
+    <LocalizationProvider dateAdapter={AdapterMoment}>
+      <Provider store={store}>
+        <Component {...pageProps} />
+      </Provider>
+    </LocalizationProvider>
+  )
 }
 
-export default MyApp;
+export default MyApp
