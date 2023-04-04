760d01cf892c7abd9b3971957d71b1a5555ad084
Voltaire-Perrochaud <contact.voltaireperrochaud@gmail.com>
2023-04-04 14:44:26 +0200

    Add persistance to store

--- front/src/pages/_app.tsx
+++ front/src/pages/_app.tsx
@@ -1,16 +1,19 @@
 import '../../styles/globals.css'
 
 import { Provider } from 'react-redux'
-import { store } from '../store/store'
 import type { AppProps } from 'next/app'
+import { persistor, store } from '../store/store'
 import { LocalizationProvider } from '@mui/x-date-pickers'
+import { PersistGate } from 'redux-persist/integration/react'
 import { AdapterMoment } from '@mui/x-date-pickers/AdapterMoment'
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <LocalizationProvider dateAdapter={AdapterMoment}>
       <Provider store={store}>
-        <Component {...pageProps} />
+        <PersistGate loading={null} persistor={persistor}>
+          <Component {...pageProps} />
+        </PersistGate>
       </Provider>
     </LocalizationProvider>
   )
