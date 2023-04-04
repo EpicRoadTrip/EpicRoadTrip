cb1061a9f0a62d8ac3b60a00e652456373c92f1e
Voltaire-Perrochaud <contact.voltaireperrochaud@gmail.com>
2023-04-04 14:44:06 +0200

    Add persistance to reducer / store

--- front/src/store/store.ts
+++ front/src/store/store.ts
@@ -1,13 +1,25 @@
+import storage from './storage';
 import { configureStore } from '@reduxjs/toolkit'
 import dateSearchSlice from './slices/dateSearchSlice';
 import { viewSlice } from './slices/viewSlice';
+import { persistReducer, persistStore } from 'redux-persist';
+import thunk from 'redux-thunk';
+
+const persistConfig = {
+  key: 'root',
+  storage,
+}
+
+const persistedDateSearchReducer = persistReducer(persistConfig, dateSearchSlice);
 
 export const store = configureStore({
   reducer: {
     view: viewSlice.reducer,
-    dateSearch: dateSearchSlice
+    dateSearch: persistedDateSearchReducer,
   },
+  middleware: [thunk]
 })
 
+export const persistor = persistStore(store);
 export type RootState = ReturnType<typeof store.getState>
 export type AppDispatch = typeof store.dispatch
