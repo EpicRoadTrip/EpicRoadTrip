ee55af68df85a0388c1eeb764f829924560000ad
Quentin <q.andreani@gmail.com>
2023-04-04 14:48:15 +0000

    Ajout de la gestion du temps de transport

--- back/transport/controllers/TransportController.go
+++ back/transport/controllers/TransportController.go
@@ -1,9 +1,27 @@
 package controllers
 
 import (
+	"EpicRoadTrip/models"
+	"EpicRoadTrip/services"
+	"net/http"
+
 	"github.com/gin-gonic/gin"
 )
 
 func GetTransportsHandler(c *gin.Context) {
 
+	var params models.Locations
+
+	if err := c.ShouldBindJSON(&params); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+
+	var res, err = services.GetTransports(params)
+	if err != nil {
+		c.AbortWithStatusJSON(http.StatusNotFound, gin.H{"error": "Location not found"})
+		return
+	}
+
+	c.JSON(http.StatusOK, gin.H{"results": res})
 }
--- back/transport/services/transportService.go
+++ back/transport/services/transportService.go
@@ -1,5 +1,66 @@
 package services
 
-func GetTransports() {
+import (
+	"EpicRoadTrip/config"
+	"EpicRoadTrip/models"
+	"context"
+	"log"
 
+	"googlemaps.github.io/maps"
+)
+
+func GetTransports(params models.Locations) (models.Transport, error) {
+	googleKey := config.GetVarEnv()["googleKey"]
+
+	client, err := maps.NewClient(maps.WithAPIKey(googleKey))
+	if err != nil {
+		log.Fatalf("fatal error: %s", err)
+	}
+
+	modes := map[string]maps.Mode{
+		"walk":    maps.TravelModeWalking,
+		"drive":   maps.TravelModeDriving,
+		"bicycl":  maps.TravelModeBicycling,
+		"transit": maps.TravelModeTransit,
+	}
+
+	transport := models.Transport{}
+	for key, mode := range modes {
+		duration := ""
+
+		directionsRequest := &maps.DirectionsRequest{
+			Origin:      params.LocationStart,
+			Destination: params.LocationDest,
+			Mode:        mode,
+		}
+
+		resp, _, err := client.Directions(context.Background(), directionsRequest)
+		if err != nil {
+			return transport, err
+		}
+
+		//fmt.Println(resp.Results)
+
+		if len(resp) > 0 && len(resp[0].Legs) > 0 {
+			duration = resp[0].Legs[0].Duration.String()
+		}
+
+		if err != nil {
+			log.Printf("Error getting directions for mode %s: %s", key, err)
+			continue
+		}
+
+		switch key {
+		case "walk":
+			transport.Walk = duration
+		case "drive":
+			transport.Drive = duration
+		case "bicycl":
+			transport.Bicycl = duration
+		case "transit":
+			transport.Transit = duration
+		}
+	}
+
+	return transport, nil
 }
