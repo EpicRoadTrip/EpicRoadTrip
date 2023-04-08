4e2e07c0e1b68044fcd381e089d99ab725456fc7
Voltaire-Perrochaud <contact.voltaireperrochaud@gmail.com>
2023-04-08 21:05:13 +0200

    Add interaction : open / close search bar

--- front/src/components/SearchEvent.tsx
+++ front/src/components/SearchEvent.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { SearchIcon } from '@chakra-ui/icons'
-import { Button, ChakraProvider, Input, Stack } from '@chakra-ui/react'
+import { Button, ChakraProvider, IconButton, Input, Stack, useOutsideClick } from '@chakra-ui/react'
 import TuneRoundedIcon from '@mui/icons-material/TuneRounded'
 import NumberSelector from './NumberSelector'
 import InputDateRangePicker from './InputDateRangePicker'
@@ -9,9 +9,24 @@ import styles from '@styledComponentStyle/SearchEvent.module.css'
 import InputSelectDropdown from './InputSelectDropdown'
 
 export default function SearchEvent() {
+  
+  const ref = React.useRef(null)
+  const [isDropdownOpen, setDropdownOpen] = React.useState(false)
+
+  function handleDropdownOpen() {
+    if (!isDropdownOpen) {
+      setDropdownOpen(true)
+    }
+  }
+
+  useOutsideClick({
+    ref: ref,
+    handler: () => setDropdownOpen(false),
+  })
+
   return (
-    <div className={styles.seContainer} data-testid="se-container">
-      <div className={styles.seHeader}>
+    <div ref={ref} className={styles.seContainer} data-testid="se-container">
+      <div className={styles.seHeader} onClick={handleDropdownOpen}>
         <div className={styles.seHeaderGroup}>
           <ChakraProvider>
             <SearchIcon fontSize='x-large' data-testid="se-header-icon-search" />
@@ -23,73 +38,79 @@ export default function SearchEvent() {
           </ChakraProvider>
         </div>
         <div className={styles.seHeaderGroup}>
-          <TuneRoundedIcon />
+          <IconButton aria-label='tune' onClick={() => setDropdownOpen(!isDropdownOpen)}>
+            <TuneRoundedIcon />
+          </IconButton>
         </div>
       </div>
-      <div className={styles.seBody}>
-        <InputSelectDropdown
-          className={styles.seSelectType}
-          data={[
-            {
-              id: 1,
-              name: 'Logement',
-              apiName: 'accomodation',
-            },
-            {
-              id: 2,
-              name: 'Restaurant',
-              apiName: 'restaurant',
-            },
-            {
-              id: 3,
-              name: 'Bar',
-              apiName: 'bar',
-            },
-            {
-              id: 4,
-              name: 'Transport',
-              apiName: 'transport',
-            },
-          ]}
-        />
-        <NumberSelector
-          className={styles.seSelectNumberTraveler}
-          title="Voyageur"
-          items={[
-            {
-              id: 1,
-              name: 'Adulte',
-              apiName: 'adult',
-              min: 0,
-              number: 0,
-            },
-            {
-              id: 2,
-              name: 'Enfant',
-              apiName: 'children',
-              min: 0,
-              number: 0,
-            },
-          ]}
-        />
-        <InputDateRangePicker className={styles.seDatePicker} />
-        <div className={styles.seBodyButtonAction}>
-          <ChakraProvider>
-            <Stack direction="column" align="center">
-              <Button
-                size="sm"
-                borderRadius={15}
-                leftIcon={<SaveOutlined htmlColor="#fff" fontSize="small" />}
-                colorScheme="blue"
-                isDisabled={true}>
-                Save search
-              </Button>
-              <Button size="lg" width={150} colorScheme="blue" isDisabled={true}>
-                Search
-              </Button>
-            </Stack>
-          </ChakraProvider>
-        </div>
+      <div className={`${styles.seBody} ${isDropdownOpen ? styles.open : ""}`}>
+        {isDropdownOpen ? (
+          <>
+            <InputSelectDropdown
+              className={styles.seSelectType}
+              data={[
+                {
+                  id: 1,
+                  name: 'Logement',
+                  apiName: 'accomodation',
+                },
+                {
+                  id: 2,
+                  name: 'Restaurant',
+                  apiName: 'restaurant',
+                },
+                {
+                  id: 3,
+                  name: 'Bar',
+                  apiName: 'bar',
+                },
+                {
+                  id: 4,
+                  name: 'Transport',
+                  apiName: 'transport',
+                },
+              ]}
+            />
+            <NumberSelector
+              className={styles.seSelectNumberTraveler}
+              title="Voyageur"
+              items={[
+                {
+                  id: 1,
+                  name: 'Adulte',
+                  apiName: 'adult',
+                  min: 0,
+                  number: 0,
+                },
+                {
+                  id: 2,
+                  name: 'Enfant',
+                  apiName: 'children',
+                  min: 0,
+                  number: 0,
+                },
+              ]}
+            />
+            <InputDateRangePicker className={styles.seDatePicker} />
+            <div className={styles.seBodyButtonAction}>
+              <ChakraProvider>
+                <Stack direction="column" align="center">
+                  <Button
+                    size="sm"
+                    borderRadius={15}
+                    leftIcon={<SaveOutlined htmlColor="#fff" fontSize="small" />}
+                    colorScheme="blue"
+                    isDisabled={true}>
+                    Save search
+                  </Button>
+                  <Button size="lg" width={150} colorScheme="blue" isDisabled={true}>
+                    Search
+                  </Button>
+                </Stack>
+              </ChakraProvider>
+            </div>
+          </>
+        ) : <></> }
       </div>
     </div>
   )
