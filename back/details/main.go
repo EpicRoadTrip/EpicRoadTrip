340bab2edb1689368d70453b9229084fa766f1f8
Quentin <q.andreani@gmail.com>
2023-04-09 20:00:55 +0000

    Ajout du swagger pour le microservices details

--- back/details/main.go
+++ back/details/main.go
@@ -5,17 +5,25 @@ import (
 	"fmt"
 	"os"
 
+	docs "EpicRoadTrip/docs"
+
 	"github.com/gin-gonic/gin"
+	swaggerfiles "github.com/swaggo/files"
+	ginSwagger "github.com/swaggo/gin-swagger"
 )
 
 func setupRouter() *gin.Engine {
 	r := gin.Default()
+
+	docs.SwaggerInfo.BasePath = "/"
+
 	r.GET("/ping", func(c *gin.Context) {
 		c.String(200, "pong")
 	})
 
 	r.GET("/details/:placeId", controllers.GetDetailHandler)
-	r.GET("/trip/:location", controllers.GetDetailTripHandler)
+
+	r.GET("/swagger/*any", ginSwagger.WrapHandler(swaggerfiles.Handler))
 
 	return r
 }
