29645206e44733090da1da52bead49b2055ff346
Voltaire-Perrochaud <contact.voltaireperrochaud@gmail.com>
2023-04-10 17:29:01 +0200

    Add map

--- front/src/pages/detail/[id].tsx
+++ front/src/pages/detail/[id].tsx
@@ -7,24 +7,35 @@ import { getDetail$ } from "src/store/slices/apiCallSlice";
 import Image from "next/image";
 import { ChakraProvider, Link } from "@chakra-ui/react";
 import { ExternalLinkIcon } from "@chakra-ui/icons";
+import Maps from "@components/Map";
 
 export default function Detail() {
     const router = useRouter()
+    const { id } = router.query
     const dispatch = useAppDispatch()
     const detailStore = useAppSelector(state => state.api.detail)
-    const { id } = router.query
+    const mapOptions: google.maps.MapOptions = {
+      zoom: 15,
+      streetViewControl: false,
+      draggable: false,
+      fullscreenControl: false,
+      mapTypeControl: false,
+      scrollwheel: false,
+      zoomControl: true,
+    };
+
     
     React.useEffect(() => {
       dispatch(setIsInPageDetail());
       if (id) {
-        dispatch(getDetail$(id as string)).unwrap().then(response => {
-          console.log(!response);
-        });
+        dispatch(getDetail$(id as string))
       }
     }, [dispatch, id])
 
     function displayDetail(): JSX.Element {
       if (detailStore && detailStore.place_id === id) {
+        const lat = detailStore.location.split(",")[0].trim()
+        const long = detailStore.location.split(",")[1].trim()
         return (
           <>
           <div className={styles.dWrapperLeft}>
@@ -52,7 +63,7 @@ export default function Detail() {
           <div className={styles.dWrapperRight}>
             <div className={styles.dWrapperRightItem}>
               <h3 className={styles.dWrapperRightItemTitle}>Localisation</h3>
-              <p>{detailStore.location}</p>
+              <Maps style={{width:400, height:250, borderRadius: 10}} lat={lat} long={long} mapOptions={mapOptions} />
             </div>
             <div className={styles.dWrapperRightItem}>
               <h3 className={styles.dWrapperRightItemTitle}>Horaire</h3>
@@ -79,10 +90,8 @@ export default function Detail() {
     }
 
     return (
-      <div className={styles.dContainer}>
       <div className={styles.dWrapper}>
         { displayDetail() }
-      </div>
       </div>
     );
 }
