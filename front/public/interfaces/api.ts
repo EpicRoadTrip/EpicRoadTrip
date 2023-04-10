813f258372f1baf366c1c64ea1cd76de672d4989
Voltaire-Perrochaud <contact.voltaireperrochaud@gmail.com>
2023-04-10 15:56:36 +0200

    Add detail info to interface

--- front/public/interfaces/api.ts
+++ front/public/interfaces/api.ts
@@ -1,11 +1,28 @@
 export interface IAPI {
     data: IDataAPI[],
     loading: loadingDetail[],
+    detail: IDetailDataAPI | null
 }
 
 export interface IDataAPI {
     id: string,
     data: IDetailAPI[]
+}
+
+export interface IDetailDataAPI {
+    place_id: string,
+    name: string,
+    formatted_address: string,
+    description: string,
+    phone: string,
+    location: string,
+    hours: {
+        id: string,
+        value: string,
+    }[],
+    opening_hours: string[],
+    website: string,
+    photo: string
 }
 
 interface IDetailAPI {
