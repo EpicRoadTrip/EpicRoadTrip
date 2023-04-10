ca3d3dea87e91dbf7c2bb09cb1b921a06ac99019
Voltaire-Perrochaud <contact.voltaireperrochaud@gmail.com>
2023-04-10 19:14:43 +0200

    Add route config

--- front/src/components/Map.tsx
+++ front/src/components/Map.tsx
@@ -1,28 +1,28 @@
 import React from "react";
 import { IMap } from "@interfaces/map";
 
-const Maps = ({style, lat, long, mapOptions}: IMap) => {
+const Maps = ({style, position, mapOptions}: IMap) => {
   const mapRef = React.useRef(null);
   React.useEffect(() => {
     let google = window.google;
     let map = mapRef.current as unknown as HTMLElement;
-    const myLatlng = new google.maps.LatLng(Number.parseFloat(lat), Number.parseFloat(long));
+    const myLatlngDepart = new google.maps.LatLng(Number.parseFloat(position.depart.lat), Number.parseFloat(position.depart.long));
+
     const mapOptionsReformated: google.maps.MapOptions = {...mapOptions,
-      center: myLatlng
+      center: myLatlngDepart
     }
     let mapGoogle = new google.maps.Map(map, mapOptionsReformated);
 
-
     const marker = new google.maps.Marker({
-      position: myLatlng,
+      position: myLatlngDepart,
       map: mapGoogle,
       animation: google.maps.Animation.DROP,
       title: "Material Dashboard React!",
     });
 
     const contentString =
-      '<div class="info-window-content"><h2>Material Dashboard React</h2>' +
-      "<p>A premium Admin for React, Material-UI, and React Hooks.</p></div>";
+      `<div class="info-window-content"><h2>${position.depart.data?.name ?? "EpiRoadTrip"}</h2>` +
+      `<p>${position.depart.data?.description ?? "No description available"}</p></div>`;
 
     const infowindow = new google.maps.InfoWindow({
       content: contentString,
@@ -31,6 +31,28 @@ const Maps = ({style, lat, long, mapOptions}: IMap) => {
     google.maps.event.addListener(marker, "click", function () {
       infowindow.open(mapGoogle, marker);
     });
+
+    if (position.dest) {
+      const myLatlngDest = new google.maps.LatLng(Number.parseFloat(position.dest.lat), Number.parseFloat(position.dest.long));
+
+      const markerDest = new google.maps.Marker({
+        position: myLatlngDest,
+        map: mapGoogle,
+        animation: google.maps.Animation.DROP,
+        title: position.dest.data?.name ?? "EpiRoadTrip",
+      });
+
+      const contentStringDest = `<div class="info-window-content"><h2>${position.dest.data?.name ?? "EpiRoadTrip"}</h2>` +
+      `<p>${position.dest.data?.description ?? "No description available"}</p></div>`;
+
+      const infowindowDest = new google.maps.InfoWindow({
+        content: contentStringDest
+      });
+
+      google.maps.event.addListener(markerDest, 'click', () => {
+        infowindowDest.open(mapGoogle, markerDest)
+      });
+    }
   });
   return (
     <>
