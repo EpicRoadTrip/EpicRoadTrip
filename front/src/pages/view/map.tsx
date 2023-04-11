0d1f7b87a04d953f44e4ecc2d6e528b0b6e7a6b8
Voltaire-Perrochaud <contact.voltaireperrochaud@gmail.com>
2023-04-11 12:38:09 +0200

    Stylish map view

--- front/src/pages/view/map.tsx
+++ front/src/pages/view/map.tsx
@@ -1,14 +1,16 @@
 import { useLoadScript, GoogleMap, MarkerF } from '@react-google-maps/api';
 import type { NextPage } from 'next';
 import { useMemo, useState } from 'react';
-import CardColumn from '@components/CardColumn';
+import CardRow from '@components/CardRow';
+import { useAppSelector } from 'src/store/hook';
+import styles from '@styledPageStyle/Map.module.css'
 
 const libraries: ("places" | "drawing" | "geometry" | "localContext" | "visualization")[] = ['places']
 
 const MapView: NextPage = () => {
     const [located, setLocated] = useState<boolean>(false);
     const [position, setPosition] = useState<google.maps.LatLng>(new google.maps.LatLng(0, 0));
-
+    const apiStore = useAppSelector(state => state.api)
     const mapOptions = useMemo<google.maps.MapOptions>(() => ({
       disableDefaultUI: false,
       clickableIcons: false,
@@ -30,17 +32,30 @@ const MapView: NextPage = () => {
         return <p>Loading...</p>;
 
     return (
-        <div style={{display: 'flex', flexDirection: 'row', width: '100%', height: '100%'}}>
-            <div style={{overflowY: 'scroll', height: (window.innerHeight/100)*90, width: (window.innerWidth/100)*14, scrollbarColor: 'red'}}>
-                <CardColumn title='Event' price='120€' description='Bar à pute'/>
-                <CardColumn title='Event' price='120€' description='Bar à pute'/>
-                <CardColumn title='Event' price='120€' description='Bar à pute'/>
-                <CardColumn title='Event' price='120€' description='Bar à pute'/>
-                <CardColumn title='Event' price='120€' description='Bar à pute'/>
-                <CardColumn title='Event' price='120€' description='Bar à pute'/>
-                <CardColumn title='Event' price='120€' description='Bar à pute'/>
-                <CardColumn title='Event' price='120€' description='Bar à pute'/>
-                <CardColumn title='Event' price='120€' description='Bar à pute'/>
+        <div className={styles.mContainer}>
+            <div className={styles.mListOfCardContainer}>
+                {
+                    apiStore.data.map((item) => (
+                        <div key={item.id} className={styles.lItem}>
+                            {
+                                item.id !== 'Transports' && item.data.length && (
+                                    <>
+                                        <h3 className={styles.lTitleItem}>{item.id}</h3>
+                                        <div className={styles.lItemContainer}>
+                                            {
+                                                item.data && item.data.length && item.data.map((value) => (
+                                                    <div key={value.place_id}>
+                                                        <CardRow id={value.place_id} title={value.name} description={value.description} imgSrc={value.photo} price={value.price_level ?? ""}  />
+                                                    </div>
+                                                ))
+                                            }
+                                        </div>
+                                    </>
+                                )
+                            }
+                        </div>
+                    ))
+                }
             </div>
             <div>
                 <GoogleMap
@@ -48,7 +63,7 @@ const MapView: NextPage = () => {
                     zoom={15}
                     center={position}
                     mapTypeId={google.maps.MapTypeId.ROADMAP}
-                    mapContainerStyle={{ width: (window.innerWidth/100)*86, height: (window.innerHeight/100)*90 }}
+                    mapContainerStyle={{ width: '100%', height: '100%' }}
                     onLoad={() => console.log('Map Component Loaded...')}
                 >
                     <MarkerF position={position}/>
