cfd1ff23fd09aaaf0362f794ad178c7c6c414a85
Voltaire-Perrochaud <contact.voltaireperrochaud@gmail.com>
2023-04-11 11:51:58 +0200

    Remove unused page / add link to home in title

--- front/src/components/Header.tsx
+++ front/src/components/Header.tsx
@@ -12,11 +12,9 @@ export default function Header() {
     return (
         <header className={viewStore.value === 'list' && !viewStore.isInPageDetail  ? styles.hhContainerImage : styles.hhContainerNoImage} style={{zIndex: '1'}}>
             <div className={ styles.hhTop}>
-                <h1 data-testid='h-title' className={styles.hhTitle}>EpicRoadTrip</h1>
+                <h1 data-testid='h-title' className={styles.hhTitle}><Link href='/'>EpicRoadTrip</Link></h1>
                 <nav style={{flex: 1, textAlign: 'center'}} className={styles.hNav}>
                     <Link href='/' style={{marginRight: '1%', color: router.pathname === '/' ? '#fff' : viewStore.value ? '#fff' : '#979DAC', borderBottom: router.pathname === '/' ? '1px solid #fff' : 'none'}} data-testid='h-nav-home'>Home</Link>
-                    <Link href='/about' style={{color: router.pathname === '/about' ? '#fff' : viewStore.value ? '#fff' : '#979DAC', borderBottom: router.pathname === '/about' ? '1px solid #fff' : 'none'}} data-testid='h-nav-about'>About us</Link>
-                    <Link href='/destinations' style={{marginLeft: '1%', color: router.pathname === '/destinations' ? '#fff' : viewStore.value ? '#fff' : '#979DAC', borderBottom: router.pathname === '/destination' ? '1px solid #fff' : 'none'}} data-testid='h-nav-destinations'>Destinations</Link>
                 </nav>
                 <div style={{flex: 1, textAlign: 'right', marginRight: '2%', color: '#fff'}} data-testid='h-copyright'>Copryright 2023</div>
             </div>
